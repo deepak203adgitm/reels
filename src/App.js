aa5d5bba30d298c680f8950c9729944163a78e0d
deepak203adgitm <73418732+deepak203adgitm@users.noreply.github.com>
2021-07-19 10:15:13 -0700

    route added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Feeds from './Components/Feeds';
 import Header from './Components/Header';
 import Login from './Components/Login';
 import Profile from './Components/Profile';
 import Signup from './Components/Signup';
+import { AuthContext, AuthProvider } from "./context/AuthProvider";
 
 
 function App() {
@@ -16,17 +17,31 @@ function App() {
       <Router>
         <Switch>
 
-   <Route path ='/login' component ={Login}></Route>
-   <Route path ='/feeds' component ={Feeds}></Route>
-   <Route path ='/profile' component ={Profile}></Route>
-   <Route path ='/signup' component ={Signup}></Route>
-      </Switch>
+          <Route path='/login' component={Login} exact ></Route>
+          <Route path='/' component={Feeds} exact ></Route>
+          <PrivateRoute path="/" comp={Feeds}></PrivateRoute>
+          <PrivateRoute path="/profile" comp={Profile}></PrivateRoute>
+        </Switch>
 
       </Router>
-     
+
 
     </div>
   );
 }
+
+
+function PrivateRoute(props) {
+  let { comp: Component, path } = props;
+  // Feeds ?? loggedIn and path="/"
+  let { currentUser } = useContext(AuthContext);
+ 
+  return currentUser ? (
+    <Route path={path} component={Component}></Route>
+  ) : (
+    <Redirect to="/login"></Redirect>
+  );
+}
+
 
 export default App;
