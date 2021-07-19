67dfc6d9810375b57c50bbfa5a9a52d307e69939
deepak203adgitm <73418732+deepak203adgitm@users.noreply.github.com>
2021-07-19 08:29:12 -0700

    route added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
+
+import Feeds from './Components/Feeds';
+import Header from './Components/Header';
+import Login from './Components/Login';
+import Profile from './Components/Profile';
+import Signup from './Components/Signup';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header> </Header>
+
+      <Router>
+        <Switch>
+
+   <Route path ='/login' component ={Login}></Route>
+   <Route path ='/feeds' component ={Feeds}></Route>
+   <Route path ='/profile' component ={Profile}></Route>
+   <Route path ='/signup' component ={Signup}></Route>
+      </Switch>
+
+      </Router>
+     
+
     </div>
   );
 }
---
+++ src/Components/Feeds.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+
+const Feeds=() => {
+    return ( <div> feed</div> );
+}
+ 
+export default Feeds;
